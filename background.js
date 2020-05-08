518752a374fc68a5de06da51dd70a691dcf43e99
lisp719 <lisp719@gmail.com>
2020-05-08 21:37:58 +0900

    Change the match condition of the URL

--- background.js
+++ background.js
@@ -1,8 +1,9 @@
 chrome.browserAction.onClicked.addListener((tab) => {
-  const { origin, pathname } = new URL(tab.url);
+  const { pathname } = new URL(tab.url);
+  const q = pathname.split("/")[2];
 
   chrome.tabs.create({
     index: tab.index + 1,
-    url: `https://socialblade.com/youtube/s/?q=${origin}${pathname}`,
+    url: `https://socialblade.com/youtube/s/?q=${q}`,
   });
 });
