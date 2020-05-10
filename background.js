c8379d69d708a13598b7e6b865a3c6681d46305d
lisp719 <lisp719@gmail.com>
2020-05-10 21:47:56 +0900

    Add context menu

--- background.js
+++ background.js
@@ -1,9 +1,34 @@
-chrome.browserAction.onClicked.addListener((tab) => {
-  const { pathname } = new URL(tab.url);
+const createTab = ({ i, url }) => {
+  const { pathname } = new URL(url);
   const q = pathname.split("/")[2];
 
   chrome.tabs.create({
-    index: tab.index + 1,
+    index: i,
     url: `https://socialblade.com/youtube/s/?q=${q}`,
+  });
+};
+
+chrome.browserAction.onClicked.addListener((tab) => {
+  createTab({ i: tab.index + 1, url: tab.url });
+});
+
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+  if (info.menuItemId === "linkUrl") {
+    createTab({ i: tab.index + 1, url: info.linkUrl });
+  } else if (info.menuItemId === "pageUrl") {
+    createTab({ i: tab.index + 1, url: info.pageUrl });
+  }
+});
+
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.contextMenus.create({
+    id: "linkUrl",
+    title: "Send the link url",
+    contexts: ["link"],
+  });
+  chrome.contextMenus.create({
+    id: "pageUrl",
+    title: "Send the page url",
+    contexts: ["page"],
   });
 });
